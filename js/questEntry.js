037c31ecb1ef8d870bb0f85b582a6ff62581fd52
mainstringargs <mainstringargs@users.noreply.github.com>
2018-03-28 15:10:44 -0500

    Update questEntry.js

--- js/questEntry.js
+++ js/questEntry.js
@@ -57,7 +57,7 @@ function selectCheckBox(questId, entryId) {
 function loadQuestEntry(entryId) {
 
     var queryListJsonRequest = new XMLHttpRequest();
-    var queryListUrl = "https://cdn.rawgit.com/mainstringargs/quest-conqueror/master/json/queryList.json";
+    var queryListUrl = "https://rawgit.com/mainstringargs/quest-conqueror/master/json/quests/questList.json";
 
 
     queryListJsonRequest.onreadystatechange = function() {
