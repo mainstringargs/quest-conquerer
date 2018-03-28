f651d3d53b16760c5f4e48576586117d754b9868
mainstringargs <mainstringargs@users.noreply.github.com>
2018-03-28 15:41:09 -0500

    Update questEntry.js

--- js/questEntry.js
+++ js/questEntry.js
@@ -128,8 +128,8 @@ function loadQuestEntry(entryId) {
 
                 var adDivData = '<div id="amzn-assoc-ad-39580e4c-818e-48d7-82cd-2422456ed385"></div>'
                 entryHtml += adDivData;
-                                     console.log("4a "+"questEntryDiv-" + entryId);
-                document.getElementById("questEntryDiv-" + entryId).innerHTML = entryHtml;
+                                     console.log("4a "+"questEntryDiv-" + questId);
+                document.getElementById("questEntryDiv-" + questId).innerHTML = entryHtml;
                 loadJS('//z-na.amazon-adsystem.com/widgets/onejs?MarketPlace=US&adInstanceId=39580e4c-818e-48d7-82cd-2422456ed385', document.body);
 
                      console.log("5 "+entryHtml);
