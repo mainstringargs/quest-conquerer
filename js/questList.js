adf01baabbaf87c002c04a64cfeaf3726a9baf14
mainstringargs <mainstringargs@users.noreply.github.com>
2018-03-28 11:15:18 -0500

    Update questList.js

--- js/questList.js
+++ js/questList.js
@@ -48,7 +48,7 @@ xmlhttp.onreadystatechange = function() {
 	}
        var adDivData ='<div id="amzn-assoc-ad-39580e4c-818e-48d7-82cd-2422456ed385"></div>'
        document.getElementById("adDiv").innerHTML = adDivData;
-       loadJS('//z-na.amazon-adsystem.com/widgets/onejs?MarketPlace=US&adInstanceId=39580e4c-818e-48d7-82cd-2422456ed385"',document.body);
+       loadJS('//z-na.amazon-adsystem.com/widgets/onejs?MarketPlace=US&adInstanceId=39580e4c-818e-48d7-82cd-2422456ed385',document.body);
 };
 xmlhttp.open("GET", url, true);
 xmlhttp.send();
